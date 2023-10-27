ca5828ce9f83bb5e5dceebd88877d782ba4d7d95
Miluchavezz <miluchavez2005@gmail.com>
2023-10-27 13:20:15 -0300

    se pone todo en un solo js

--- js/paginaPrincipal.js
+++
@@ -1,6 +0,0 @@
-var guardadoDeUsuario=localStorage.getItem("usuario");
-
-if(guardadoDeUsuario){
-    let textoACambiar=document.getElementById('nombre-usuario');
-    textoACambiar.textContent=guardadoDeUsuario;
-}
