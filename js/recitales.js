7d14ac783bb348dfb42c50858bf546ec7e4d8352
Miluchavezz <miluchavez2005@gmail.com>
2023-11-03 16:47:58 -0300

    slider

--- js/recitales.js
+++ js/recitales.js
@@ -4,4 +4,26 @@ let button = document.getElementById("cross");
 
 button.addEventListener("click",()=> {
     popUp.classList.add("hidden");
-})
+});
+
+const slider = document.querySelector('.recitales_slider_inner');
+const images = slider.querySelectorAll('img');
+let index = 0;
+
+function nextSlide() {
+    index = (index + 1) % images.length;
+    updateSlider();
+}
+
+function prevSlide() {
+    index = (index - 1 + images.length) % images.length;
+    updateSlider();
+}
+
+function updateSlider() {
+    const percentage = index * -100;
+    slider.style.transform = `translateX(${percentage}%)`;
+}
+
+setInterval(nextSlide, 3000); 
+
