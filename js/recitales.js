780e216423380a06cfc25a8823aedb3988bfce55
Ezequiel Miranda <mirandaezequiel522@gmail.com>
2023-11-05 16:05:42 -0300

    "calendario"

--- js/recitales.js
+++ js/recitales.js
@@ -6,6 +6,7 @@ button.addEventListener("click",()=> {
     popUp.classList.add("hidden");
 });
 
+// SLIDER
 const slider = document.querySelector('.recitales_slider_inner');
 const images = slider.querySelectorAll('img');
 let index = 0;
@@ -27,3 +28,36 @@ function updateSlider() {
 
 setInterval(nextSlide, 3000); 
 
+// CALENDARIO
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendario');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['dayGrid'],
+        locale: 'es',
+        events: [
+            {
+                title: 'Pink Floyd',
+                start: '2023-11-22',
+                end: '2023-11-23',
+            },
+            {
+                title: 'Tan Bionica',
+                start: '2023-11-29',
+                end: '2023-11-30',
+            },
+            {
+                title: 'Taylor Swift',
+                start: '2023-12-3',
+                end: '2023-12-4',
+            },
+            {
+                title: 'Red Hot Chili Peppers',
+                start: '2023-12-15',
+                end: '2023-12-16',
+            },
+        ],
+    });
+
+    calendar.render();
+});
